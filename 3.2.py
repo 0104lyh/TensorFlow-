6ba18338b3a4c7b87a57552fe2fcd9a328669567
0104lyh <48237468+0104lyh@users.noreply.github.com>
2019-03-29 22:24:58 +0800

    Add files via upload

---
+++ 3.2.py
@@ -0,0 +1,16 @@
+#简单神经网络
+import tensorflow as tf
+#传入参数x
+x = tf.constant([[0.7,0.5]])
+w1=tf.Variable(tf.random_normal([2,3],stddev=1,seed=1))
+w2=tf.Variable(tf.random_normal([3,1],stddev=1,seed=1))
+
+#定义前向传播过程
+a=tf.matmul(x,w1)
+y=tf.matmul(a,w2)
+
+#用会话计算结果
+with tf.Session() as sess:
+	init_op = tf.global_variables_initializer()
+	sess.run(init_op)
+	print("y in 3.2.py is:\n",sess.run(y))
